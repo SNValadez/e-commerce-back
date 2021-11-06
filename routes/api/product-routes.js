44243f87a964f2715f2b3929fbca1b53490954a7
SNValadez <snvaladez13@gmail.com>
2021-11-05 23:32:06 -0700

    router.get findOne product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,6 +28,36 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ["id"],
+    include: [{
+      model: Category,
+      attributes: ["category_name"]
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"]
+      }
+
+    ]
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({
+        message: "No product with this id"
+      });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
   // be sure to include its associated Category and Tag data
 });
 
